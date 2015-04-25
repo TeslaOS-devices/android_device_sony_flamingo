f828339b55d6c30422a6315dfa72e0e6b28dda94
NoelMacwan <yulespencer@gmail.com>
2015-04-25 23:05:27 +0530

    Tesla Bringup
    Signed-off-by: NoelMacwan <yulespencer@gmail.com>

--- tesla.mk
+++ tesla.mk
@@ -32,12 +32,12 @@ $(call inherit-product, device/sony/flamingo/aosp_d2203.mk)
 $(call inherit-product, device/sony/common/resources.mk)
 
 # Inherit CM common stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=D2203 \
     BUILD_FINGERPRINT=Sony/D2203/D2203:4.4.2/18.4.C.1.29/4nv_bQ:user/release-keys \
     PRIVATE_BUILD_DESC="D2203-user 4.4.2 18.4.C.1.29 4nv_bQ release-keys"
 
-PRODUCT_NAME := cm_flamingo
+PRODUCT_NAME := tesla_flamingo
 PRODUCT_DEVICE := flamingo
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,4 +15,4 @@
 #
 
 add_lunch_combo aosp_d2203-userdebug
-add_lunch_combo cm_flamingo-userdebug
+add_lunch_combo tesla_flamingo-userdebug
